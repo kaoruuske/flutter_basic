a2ad2f93d4a2e630aacde66c2fc2dbf43fd7f526
kaoruuske <kaoruuske@naver.com>
2019-08-19 13:02:52 +0900

    GIT 설정 변경 후 재 등록

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,35 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-      ),
-      //home: MyHomePage(title: 'Flutter Demo Home Page'),
-      home: Text('헬로 월드'),
-    );
+      ), //ThemeData
+      home: HelloPage('Hello World')
+//      home: Scaffold(
+//        appBar: AppBar(title: Text('Hello World'),
+//        ), //AppBar
+//          body: Text('헬로 월드', style: TextStyle(fontSize: 30),
+//          )), //Text, Scaffold
+    ); //MaterialApp
+  }
+}
+
+class HelloPage extends StatefulWidget {
+
+  final String title;
+
+  HelloPage(this.title);
+
+  @override
+  _HelloPageState createState() => _HelloPageState();
+}
+
+class _HelloPageState extends State<HelloPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(widget.title),
+      ), //appbar
+        body: Text(widget.title, style: TextStyle(fontSize: 30)));
   }
 }
